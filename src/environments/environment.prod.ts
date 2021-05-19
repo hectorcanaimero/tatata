f813d74bfd1d4767e565329bef5f4ef484daa305
Hector Velasques <hector.velasques@condor.com.br>
2021-05-19 10:59:56 -0300

    entre posto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,5 @@ export const environment = {
   v3: 'https://teste.condor.com.br/news/wp-json/wp/v2',
   v4: 'https://teste.condor.com.br/blog/wp-json/wp/v2',
   v5: 'https://marketing.condor.com.br/api/Containers/produtos/download',
-  name: 'passeio-ciclistico'
+  name: 'entre-posto'
 };
