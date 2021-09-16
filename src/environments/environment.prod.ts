53d83d9238bcf23ae0f0c87f035d8c01a0a56ff2
Hector Velasques <hector.velasques@condor.com.br>
2021-09-16 09:50:00 -0300

    Config do slug Aniversario

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
-  v1: 'https://marketing.condor.com.br/api',
+  name: 'anicversario',
   v2: 'https://painel.condor.com.br',
+  v1: 'https://marketing.condor.com.br/api',
   v3: 'https://teste.condor.com.br/news/wp-json/wp/v2',
   v4: 'https://teste.condor.com.br/blog/wp-json/wp/v2',
   v5: 'https://marketing.condor.com.br/api/Containers/produtos/download',
-  name: 'dia-dos-pais'
 };
